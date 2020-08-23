c3b281da081a74b8b292a5c355be338ee4e302d6
chukwuemeka torti <emeka.torti@outlook.com>
2020-08-23 12:24:48 -0400

    Deleted Data and migration folders from main project and created migrations in Data project

---
+++ BookListRazor.Data/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BookListRazor.Data.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("Book Name")]
+        public string Name { get; set; }
+        public string Author { get; set; }  
+    }
+}
--- BookListRazor/Startup.cs
+++ BookListRazor/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using BookListRazor.Data;
+using BookListRazor.Data.Context;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
