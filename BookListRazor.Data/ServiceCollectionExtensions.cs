6c12c8298c4f797d1c4f0641ced5ba9a199767b5
chukwuemeka torti <emeka.torti@outlook.com>
2020-08-23 09:31:22 -0400

    Added Data project to solution

---
+++ BookListRazor.Data/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BookListRazor.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BookListRazor.Data
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection RegisterDataServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")));
+
+
+            return services;
+        }
+    }
+}
