89c5ea17498a93416ab9d53952116bca3fc3db07
chukwuemeka torti <emeka.torti@outlook.com>
2020-08-23 14:35:06 -0400

    Added Book front end

---
+++ BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Data.Context;
+using BookListRazor.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db, ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+        public async Task OnGet()
+        {
+            Books = await _db.Book.ToListAsync();
+        }
+    }
+}
