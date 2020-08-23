cdc25404b0987d7a818220d5e149c98b4e9d9f35
chukwuemeka torti <emeka.torti@outlook.com>
2020-08-23 12:33:01 -0400

    Created Book model and added to DB

--- BookListRazor.Data/Context/ApplicationDbContext.cs
+++ BookListRazor.Data/Context/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BookListRazor.Data.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,7 @@ namespace BookListRazor.Data.Context
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Book> Book { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
